2de4019078aed42c7d5b85aaaf8c25192738e944
Devin Amlen <devinamlen@gmail.com>
2023-09-23 15:58:46 -0400

    Finishing touches

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const data = await Tag.findAll({
-      include: [{ model: Product }, {model: ProductTag}]
+      include: [{ model: Product }]
     });
     res.status(200).json(data);
   } catch (err) {
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const data = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }]
+      include: [{ model: Product }]
     });
     if (!data) {
       res.status(404).json({ message: 'No tag found with that id!' });
